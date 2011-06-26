40a6d81b79521f80d8b7bc5aa464950916510d8a
chrigu <chmuehlethaler@freesurf.ch>
2011-06-26 15:43:16 +0200

    added .idea and .bundle to gitignore

--- test/unit/plants_beds_test.rb
+++ test/unit/plants_beds_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PlantsOfBedsTest < ActiveSupport::TestCase
+class PlantsBedsTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
